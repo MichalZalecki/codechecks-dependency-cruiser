dff35a629927032d57453481ed9237946d48618b
Michał Załęcki <michal@michalzalecki.com>
2019-09-18 19:57:37 +0200

    Graphs (#2)
    * Save graphs
    
    * Save artifact
    
    * Bump version
    
    * Bump version
    
    * Bump version
    
    * Do not show graph path in local mode
    
    * Bump version
    
    * Bump version
    
    * Update README

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ checks:
   - name: codechecks-dependency-cruiser
     options:
       exclude: (node_modules)
+      graph: true
       paths:
         - src
 ```
@@ -18,9 +19,21 @@ Options
 | name | required | description |
 | --- | --- | --- |
 | `paths` | yes | files or directories to cruise |
+| `graph` | no | whether to generate dependency graph |
 | `exclude` | no | a regexp for modules to exclude from being cruised |
 | `config` | no | custom path to .dependency-cruiser.js or .dependency-cruiser.json |
+
+## Install GraphViz
+
+macOS
+
+    brew install graphviz
+
+Ubuntu
+
+    sudo apt-get install graphviz
 
 ## Limitations
 
 * Extending presets is not supported, use self-contained dependency-cruiser configuration
+* Generating graphs require [GraphViz](http://www.graphviz.org/) installed
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as path from "path";
+import * as os from "os";
+import { execSync } from "child_process";
 import { codechecks, CodeChecksReport } from "@codechecks/client";
-import { cruise, IForbiddenRuleType } from "dependency-cruiser";
+import { cruise } from "dependency-cruiser";
 import { pluralize } from "./utils";
 
 interface DependencyCruiserOptions {
   paths: string[];
+  graph?: boolean;
   exclude?: string;
   config?: string;
 }
@@ -32,20 +35,56 @@ function readRuleSet(configFilePath?: string): { [key: string]: any } {
     return require(path.resolve(configFilePath));
   } else if (canRequire(path.resolve(".dependency-cruiser.js"))) {
     return require(path.resolve(".dependency-cruiser.js"));
-  } else if (canRequire(path.resolve(".dependency-cruiser.js"))) {
-    return require(path.resolve(".dependency-cruiser.js"));
+  } else if (canRequire(path.resolve(".dependency-cruiser.json"))) {
+    return require(path.resolve(".dependency-cruiser.json"));
   }
   throw new Error("dependency-cruiser config not found");
+}
+
+function createGraph(options: DependencyCruiserOptions): string {
+  const result = cruise(options.paths, {
+    exclude: options.exclude,
+    outputType: "dot"
+  });
+
+  const { output } = result;
+
+  const GRAPH_NAME = "dependencygraph.png";
+
+  const graphPath = path.join(os.tmpdir(), GRAPH_NAME);
+
+  execSync(`echo ${JSON.stringify(output)} | dot -T png > ${graphPath}`);
+
+  return graphPath
+}
+
+async function saveArtifact(name: string, path: string) {
+  await codechecks.saveFile(name, path);
+
+  const artifactLink = codechecks.getArtifactLink(name);
+
+  return artifactLink;
 }
 
 async function dependencyCruiser(options: DependencyCruiserOptions): Promise<void> {
   const ruleSet = readRuleSet(options.config);
 
-  const { output } = cruise(options.paths, {
+  const result = cruise(options.paths, {
     exclude: options.exclude,
     validate: true,
     ruleSet,
   });
+
+  let graphPath: string | undefined;
+  let artifactLink: string | undefined;
+
+  if (options.graph) {
+    graphPath = createGraph(options);
+    const artifactName = path.parse(graphPath).base;
+    artifactLink = await saveArtifact(artifactName, graphPath);
+  }
+
+  const { output } = result;
 
   const errorsText = pluralize(output.summary.error, ["error", "errors"]);
   const warnsText = pluralize(output.summary.warn, ["warn", "warns"]);
@@ -57,14 +96,16 @@ async function dependencyCruiser(options: DependencyCruiserOptions): Promise<voi
 | :--: | :-------: | :------: |
 ${output.summary.violations
   .map((v: Violation) => `| ${v.from} | ${v.rule.name} | ${v.rule.severity} |`)
-  .join("\n")}
-  `;
+  .join("\n")}${graphPath && codechecks.context.isLocalMode ? `
+
+Graph generated: ${graphPath}` : ""}`;
 
   const report: CodeChecksReport = {
     name: "Dependency Cruiser",
     shortDescription,
     longDescription,
     status: output.summary.error > 0 ? "failure" : "success",
+    detailsUrl: artifactLink,
   };
 
   codechecks.report(report);
